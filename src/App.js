d70d9f3da0007c67a92e302b1ad433a8d8543fb3
Brogs <brogan.bennett@svu.edu>
2024-03-14 12:06:48 -0400

    i added in a complaint list file and added it to the header

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import TabbedLayout from './layout/tabbed-layout'
 import ComplaintDashboard from './pages/complaint-dashboard'
 import ComplaintWorkflow from './pages/complaint-workflow'
 import SubmitComplaint from './pages/submit-complaint'
+import ComplaintList from './pages/complaint-list'
 
 function App() {
-  const tabLabels = [  "Submit Complaint", "Complaint Workflow", "Complaint Dashboard", "Examples", "Old Page"]
-  const tabPanels = [  <SubmitComplaint />, <ComplaintWorkflow />, <ComplaintDashboard />, <Examples />, <OldPage />]
+  const tabLabels = [  "Submit Complaint", "Complaint Workflow", "Complaint Dashboard", "Examples", "Old Page", "Complaint List"]
+  const tabPanels = [  <SubmitComplaint />, <ComplaintWorkflow />, <ComplaintDashboard />, <Examples />, <OldPage />, <ComplaintList />]
   return (
     <>
         <TabbedLayout  
---
+++ src/pages/complaint-list.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ComplaintList = () => {
+  return (
+<h1>Complaint List</h1>
+  )
+
+}
+
+export default ComplaintList
